d467d54181edb5b34fcce638ca42c7ee01ee6890
TAPIT08 <jetapit@my.cspc.edu.ph>
2024-12-18 00:06:23 +0800

    database.py addition

---
+++ database.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('base.html')
+
+@app.route('/home')
+def home():
+    return render_template('base.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/github')
+def github():
+    return render_template('github.html')
+
+@app.route('/signin')
+def signin():
+    return render_template('signin.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+@app.route('/feedback')
+def feedback():
+    return render_template('feedback.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
