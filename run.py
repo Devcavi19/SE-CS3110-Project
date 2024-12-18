8c98a219eadf4e060014c873688dbb0a5ae64060
TAPIT08 <jetapit@my.cspc.edu.ph>
2024-12-18 12:54:25 +0800

    addition

--- run.py
+++ run.py
@@ -88,4 +88,4 @@ def signup():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()  # Create database tables
-    app.run(debug=True)
+    app.run(debug=True, port=5003)
