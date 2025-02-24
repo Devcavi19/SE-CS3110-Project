1c6bdef772f8a769cc0593dbb5cd832fcc1f807a
Herald Carl Avila <144099137+Devcavi19@users.noreply.github.com>
2025-02-24 14:35:35 +0000

    Format import statements in run.py for better readability

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ import os
 from functools import wraps
 try:
     from flask import (
-        Flask, render_template, request, redirect, 
+        Flask, render_template, request, redirect,
         url_for, flash, session
     )
     from werkzeug.security import generate_password_hash, check_password_hash
